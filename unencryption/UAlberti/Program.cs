62adfe8ad1fa58c827b4da8c15e271f5b81d2f33
Quickscoper <nikitamarkov2u@gmail.com>
2022-12-08 21:35:01 +0500

    Added Alberti Circle Unencryption

---
+++ unencryption/UAlberti/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Alberti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            string keyAlphabet = Console.ReadLine().ToLower().Replace(" ", "");
+            string encryptedText = Console.ReadLine().ToLower().Replace(" ", "");
+
+            string unencryptedText = new Func<string>(() =>
+            {
+                string returnableValue = "";
+                char bubble;
+
+                foreach (var item in encryptedText)
+                {
+                    returnableValue += alphabet[keyAlphabet.IndexOf(item)];
+                    bubble = keyAlphabet.Last();
+                    keyAlphabet = bubble + keyAlphabet.Remove(keyAlphabet.Length - 1, 1);
+                }
+                return returnableValue;
+            })();
+            Console.WriteLine(unencryptedText);
+        }
+    }
+}

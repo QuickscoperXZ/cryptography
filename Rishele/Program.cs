62ea28acef5f42f6a129c2bf51bd043f64b59f8f
QuickscoperXZ <104735790+QuickscoperXZ@users.noreply.github.com>
2022-12-11 01:27:24 +0500

    Добавлены комментарии

--- Rishele/Program.cs
+++ Rishele/Program.cs
@@ -6,19 +6,19 @@ namespace Rishele
     {
         static void Main(string[] args)
         {
-            string unencryptedText = Console.ReadLine().Replace(" ", "");
-            List<string> encryptedSubstringText = new List<string>();
-            List<List<int>> key = new List<List<int>>();
+            string unencryptedText = Console.ReadLine().Replace(" ", "");//считываем шифруемое сообщение
+            List<string> encryptedSubstringText = new List<string>(); //контейнер под разбитое сообщение
+            List<List<int>> key = new List<List<int>>(); //контейнер под ключ, т. к. ключ - набор значений, в принципе можно исопльзовать и форматированную строку
             Random rand = new Random();
             int startingIndex = 0, length;
 
-            while (startingIndex != unencryptedText.Length)
+            while (startingIndex != unencryptedText.Length) //разбиваем строку
             {
                 length = rand.Next(1, (unencryptedText.Length) - startingIndex);
                 encryptedSubstringText.Add(new string(unencryptedText.Substring(startingIndex, length)));
                 startingIndex += length;
             }            
-            for (int i = 0; i < encryptedSubstringText.Count; i++)
+            for (int i = 0; i < encryptedSubstringText.Count; i++) //генерируем основу под ключ - список списков с нетасованными индексами букв в шифруемом сообщении
             {
                 key.Add(new Func<List<int>>(() => 
                 {
@@ -29,7 +29,7 @@ namespace Rishele
                     }
                     return newList;
                 })());
-                for (int j = 0; j < encryptedSubstringText[i].Length; j++)
+                for (int j = 0; j < encryptedSubstringText[i].Length; j++) //тасуем индексы в ключе и выставляем буквы в тасованном порядке
                 {
                     char[] swap = encryptedSubstringText[i].ToCharArray();
                     int swapIndex = rand.Next(0, encryptedSubstringText[i].Length - 1);

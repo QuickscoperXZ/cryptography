cd70ca27de2fc44a07422ff03e11b5ea6ca4ae01
QuickscoperXZ <104735790+QuickscoperXZ@users.noreply.github.com>
2022-12-11 10:31:18 +0500

    Добавлены комментарии

--- unencryption/UCaesar/Program.cs
+++ unencryption/UCaesar/Program.cs
@@ -12,12 +12,12 @@ namespace Atabsh_Caesar
 
             Regex pattern = new Regex(@"[a-zA-z]"); //Регулярное выражение для проверки соответствия
 
-            for (int i = 0;i<encryptedText.Count();i++) //В цикле проверяем совпадает ли число с нашим выражением
+            for (int i = 0;i<encryptedText.Count();i++)
             {
-                if (pattern.Match(Convert.ToString(encryptedText[i])).Success) 
+                if (pattern.Match(Convert.ToString(encryptedText[i])).Success) //Проверяем совпадает ли число с нашим выражением
                 {
                     if (Convert.ToChar(Convert.ToInt32(encryptedText[i]) - key) < 97) 
-                        encryptedText[i] = Convert.ToChar(Convert.ToInt32(encryptedText[i]) + (122 -  key)); //Если совпало то меняем текущий символ на следующий
+                        encryptedText[i] = Convert.ToChar(Convert.ToInt32(encryptedText[i]) + (122 -  key)); //Если совпало то меняем текущий символ на предыдущий, если уходим за пределы алфавита в ASCII то считаем от конца алфваита
                     else
                         encryptedText[i] = Convert.ToChar(Convert.ToInt32(encryptedText[i]) + key);
                 }

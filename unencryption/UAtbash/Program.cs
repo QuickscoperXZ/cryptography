96431ecc9ad11343b455b854bad9f86a447bfde9
QuickscoperXZ <104735790+QuickscoperXZ@users.noreply.github.com>
2022-12-11 10:28:13 +0500

    Добавлены комментарии

--- unencryption/UAtbash/Program.cs
+++ unencryption/UAtbash/Program.cs
@@ -7,17 +7,17 @@ namespace Atbash
     {
         static void Main(string[] args)
         {
-            char[] encryptedText = Console.ReadLine().ToLower().ToCharArray() ?? "sampletext".ToCharArray();
-            Regex pattern = new Regex(@"[a-zA-z]");
+            char[] encryptedText = Console.ReadLine().ToLower().ToCharArray() ?? "sampletext".ToCharArray(); //считываем защифрованный текст
+            Regex pattern = new Regex(@"[a-zA-z]"); //регулярное выражение для проверки является ли текущий символ буквой
 
             for (int i = 0; i <= encryptedText.Length - 1; i++)
             {
                 if (pattern.Match(Convert.ToString(encryptedText[i])).Success)
                 {
-                    encryptedText[i] = Convert.ToChar(26 - (Convert.ToInt32(encryptedText[i]) - 96 - 1) + 96);
+                    encryptedText[i] = Convert.ToChar(26 - (Convert.ToInt32(encryptedText[i]) - 96 - 1) + 96); //сдвигаем буквы
                 }
             }
-            Console.WriteLine(new String(encryptedText));
+            Console.WriteLine(new String(encryptedText)); //выводим расшифрованное сообщение
         }
     }
-}
+} //да, это тот же самый код, в этом и суть атбаша. в алгоритме нет как такого ключа и ввиду того что алгоритм "разворачивает алфваит" развернув алфавит дважды можно получить зашифрованное сообщение
